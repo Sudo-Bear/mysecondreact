ac561b44e08a1a9ddcfa4d03ef5e995764e63170
Dylan Cross <dylancross100@gmail.com>
2020-02-13 10:05:52 -0600

    Example of React component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+class Car extends React.Component {
+  render() {
+    return <h2>Hi, I am a Car!</h2>
+  }
+}
+
 const myfirstelement = <h1>Hello React!</h1>
+const mysecondelement = <h1>Hello React... AGAIN!</h1>
 
 ReactDOM.render(myfirstelement, document.getElementById('root'));
+ReactDOM.render(mysecondelement, document.getElementById('root2'));
+ReactDOM.render(<Car />, document.getElementById('root3'));
