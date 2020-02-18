c8c46f8318e02a274213dccb264711a533b174ea
Dylan Cross <dylancross100@gmail.com>
2020-02-18 14:46:09 -0600

    Reorganized CSS into modules

--- src/App.js
+++ src/App.js
@@ -1,35 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import styles from './mystyle.module.css';
 
 class Car extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      wheels: this.props.wheels,
-      color: this.props.color,
-      brand: this.props.brand
-    }
-  }
-  changeColor = () => {
-    this.setState({color: "Orange"})
-  }
   render() {
-    return (
-      <div>
-        <ul>
-          <li>Hi, I am a Car!</li>
-          <li>I have {this.state.wheels} wheels</li>
-          <li>Model: {this.state.brand}</li>
-          <li>
-            I am painted {this.state.color} <button
-            type="button"
-            onClick={this.changeColor}
-            >Make Orange</button>
-          </li>
-        </ul>
-
-      </div>
-    )
+    return <h1 className={styles.bigblue}>Hello Car!</h1>;
   }
 }
 
