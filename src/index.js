19094513143936a333bdf3a3be9ae97e81a84979
Dylan Cross <dylancross100@gmail.com>
2020-02-18 14:10:00 -0600

    CSS Styling

--- src/index.js
+++ src/index.js
@@ -1,47 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class MyForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      username: '',
-      age: null, 
-      errormessage: ''
-    };
-  }
-  myChangeHandler = (event) => {
-    let nam = event.target.name;
-    let val = event.target.value;
-    let err = '';
-    if (nam === "age") {
-      if (val != '' && !Number(val)) {
-        err = <strong>Your age must be a number</strong>;
-      }
-    }
-    this.setState({errormessage: err});
-    this.setState({[nam]: val});
-  }
+class MyHeader extends React.Component {
   render() {
     return (
-      <form>
-        <h1>Hello {this.state.username} {this.state.age}</h1>
-        <p>Enter your name:</p>
-        <input
-          type="text"
-          name='username'
-          onChange={this.myChangeHandler}
-        />
-        <p>Enter your age:</p>
-        <input 
-          type='text'
-          name='age'
-          onChange={this.myChangeHandler}
-        />
-        {this.state.errormessage}
-      </form>
+      <div>
+        <h1 style={{backgroundColor: "lightblue"}}>Hello Style!</h1>
+        <p>Add a little style!</p>
+      </div>
     );
   }
 }
 
-ReactDOM.render(<MyForm />, document.getElementById("root"));
+ReactDOM.render(<MyHeader />, document.getElementById("root"));
