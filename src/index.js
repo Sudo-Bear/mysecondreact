725da4c2c4e030ca4298b594ab354a218263b17d
Dylan Cross <dylancross100@gmail.com>
2020-02-18 14:13:43 -0600

    Passing dicts of styling as a chunk

--- src/index.js
+++ src/index.js
@@ -3,9 +3,15 @@ import ReactDOM from 'react-dom';
 
 class MyHeader extends React.Component {
   render() {
+    const mystyle = {
+      color: "white", 
+      backgroundColor: "DodgerBlue",
+      padding: "10px",
+      fontFamily: "Arial"
+    };
     return (
       <div>
-        <h1 style={{backgroundColor: "lightblue"}}>Hello Style!</h1>
+        <h1 style={mystyle}>Hello Style!</h1>
         <p>Add a little style!</p>
       </div>
     );
